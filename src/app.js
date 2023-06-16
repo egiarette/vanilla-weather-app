6ee28624e3e44cf2167f2d1a636e70843f362359
egiarette <egiarette@gmail.com>
2023-06-16 16:44:36 -0400

    Added relevant Icon

--- src/app.js
+++ src/app.js
@@ -3,9 +3,15 @@ function displayInitial(response) {
 	let temperatureElement = document.querySelector("#temp");
 	let cityElement = document.querySelector("#city");
 	let descriptionElement = document.querySelector("#description");
+	let iconElement = document.querySelector("#icon");
 	temperatureElement.innerHTML = Math.round(response.data.main.temp);
 	cityElement.innerHTML = response.data.name;
 	descriptionElement.innerHTML = response.data.weather[0].description;
+	iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "8c48afa47a9a9c24f3500c7039d50aaa";
